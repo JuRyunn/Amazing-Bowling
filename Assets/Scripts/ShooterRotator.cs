819790e74710bb1ddec89115d3adeb619b9d69a2
JuRyunn <wngus2991@gmail.com>
2022-07-20 01:24:03 +0900

    ShooterRotateCommit

---
+++ Assets/Scripts/ShooterRotator.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShooterRotator : MonoBehaviour
+{
+    private enum RotateSate
+    {
+        Idle, Vertical, Horizontal, Ready
+    }
+
+    private RotateSate state= RotateSate.Idle;
+
+    public float verticalRotateSpeed = 360f;
+    public float horizontalRotateSpeed = 360f;
+
+    void Update()
+    {
+        if (state == RotateSate.Idle) 
+
+        {
+            if (Input.GetButtonDown("Fire1")) // 마우스를 누르는 순간
+            {
+                state = RotateSate.Horizontal; // Horizontal로 변경
+            }
+        }
+        else if (state == RotateSate.Horizontal) 
+        {
+            if (Input.GetButton("Fire1")) // 마우스를 누르고 있으면
+            {
+                // 회전한다
+                transform.Rotate(new Vector3(0, horizontalRotateSpeed * Time.deltaTime, 0));
+            }
+            else if (Input.GetButtonUp("Fire1")) // 마우스에서 손을 떼면
+            {
+                state = RotateSate.Vertical; // Horizontal에서 Vertical로 바꿔준다.
+            }
+            else if (state == RotateSate.Vertical)
+            {
+                if (Input.GetButton("Fire1")) // 마우스를 누르고 있으면
+                {
+                    // x축에 대한 회전
+                    transform.Rotate(new Vector3(-verticalRotateSpeed * Time.deltaTime, 0, 0));
+                }
+                else if (Input.GetButtonUp("Fire1")) // 마우스에서 손을 떼면
+                {
+                    state = RotateSate.Ready; // 멈춰서 대기
+                }
+            }
+        }
+    }
+
+
+
+
+}
